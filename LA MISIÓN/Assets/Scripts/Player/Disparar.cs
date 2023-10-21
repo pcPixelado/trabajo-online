7ac584ede5718b3069e10f5d2b5f6e64e16b04a9
Varano3 <117668307+Varano3@users.noreply.github.com>
2023-10-21 12:16:53 +0200

    Revert "Merge branch 'main' of https://github.com/pcPixelado/trabajo-online"
    This reverts commit 3188079722cf545dfc96de128732aedb7804ed54, reversing
    changes made to cf6ce3baf32d9eb2c95fbffeeec5835e324ecbc6.

--- LA MISIÓN/Assets/Scripts/Player/Disparar.cs
+++ LA MISIÓN/Assets/Scripts/Player/Disparar.cs
@@ -9,23 +9,10 @@ public class PlayerController : MonoBehaviour
 
     public Armas armaEquipada;
 
-    public Armas[] armasExistentes;
-
     public float timer;
     void Update()
     {
-        if (armaEquipada.Automatica)
-        {
-            if (Input.GetKey(KeyCode.Mouse0) && timer > armaEquipada.CadenciaDeTiro)
-            {
-                Shoot();
-            }
-            else if (timer <= armaEquipada.CadenciaDeTiro)
-            {
-                timer = timer + Time.deltaTime;
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.Mouse0) && timer > armaEquipada.CadenciaDeTiro)
+        if (Input.GetKeyDown(KeyCode.Mouse0) && timer > armaEquipada.CadenciaDeTiro)
         {
             Shoot();
         }
@@ -33,7 +20,6 @@ public class PlayerController : MonoBehaviour
         {
             timer = timer + Time.deltaTime;
         }
-
 
         Vector3 vectormouse = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
 
@@ -48,7 +34,7 @@ public class PlayerController : MonoBehaviour
         timer = 0;
         for (int i = 0; i < armaEquipada.NumeroDeBalasPorDisparo; i++)
         {
-            GameObject bullet = Instantiate(armaEquipada.TipoDeMunicíon, firePoint.position, Quaternion.Euler(0,0,transform.rotation.z + Random.Range(-armaEquipada.Dispersión * 2, armaEquipada.Dispersión * 2)));
+            GameObject bullet = Instantiate(armaEquipada.TipoDeMunicíon, firePoint.position, Quaternion.Euler(0, 0, transform.rotation.z + Random.Range(-armaEquipada.Dispersión, armaEquipada.Dispersión)));
             Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
             rb.velocity = firePoint.up * armaEquipada.VelocidadDeLasBalas;
 
