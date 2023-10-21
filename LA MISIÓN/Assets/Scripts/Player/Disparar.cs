bd259e943773394728586c25c73e310489b8951d
Varano3 <117668307+Varano3@users.noreply.github.com>
2023-10-21 11:30:53 +0200

    armas

--- LA MISIÓN/Assets/Scripts/Player/Disparar.cs
+++ LA MISIÓN/Assets/Scripts/Player/Disparar.cs
@@ -12,7 +12,18 @@ public class PlayerController : MonoBehaviour
     public float timer;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0) && timer > armaEquipada.CadenciaDeTiro)
+        if (armaEquipada.Automatica)
+        {
+            if (Input.GetKey(KeyCode.Mouse0) && timer > armaEquipada.CadenciaDeTiro)
+            {
+                Shoot();
+            }
+            else if (timer <= armaEquipada.CadenciaDeTiro)
+            {
+                timer = timer + Time.deltaTime;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Mouse0) && timer > armaEquipada.CadenciaDeTiro)
         {
             Shoot();
         }
@@ -20,6 +31,7 @@ public class PlayerController : MonoBehaviour
         {
             timer = timer + Time.deltaTime;
         }
+
 
         Vector3 vectormouse = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
 
@@ -34,7 +46,7 @@ public class PlayerController : MonoBehaviour
         timer = 0;
         for (int i = 0; i < armaEquipada.NumeroDeBalasPorDisparo; i++)
         {
-            GameObject bullet = Instantiate(armaEquipada.TipoDeMunicíon, firePoint.position, Quaternion.Euler(0,0,transform.rotation.z + Random.Range(-armaEquipada.Dispersión, armaEquipada.Dispersión)));
+            GameObject bullet = Instantiate(armaEquipada.TipoDeMunicíon, firePoint.position, Quaternion.Euler(0,0,transform.rotation.z + Random.Range(-armaEquipada.Dispersión * 2, armaEquipada.Dispersión * 2)));
             Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
             rb.velocity = firePoint.up * armaEquipada.VelocidadDeLasBalas;
 
