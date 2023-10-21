8fbfe40b09ef7dd56a1b11e48a5dc2693e1bc6ba
pcPixelado <147920370+pcPixelado@users.noreply.github.com>
2023-10-21 11:57:38 +0200

    Update Disparar.cs

--- LA MISIÓN/Assets/Scripts/Player/Disparar.cs
+++ LA MISIÓN/Assets/Scripts/Player/Disparar.cs
@@ -1,76 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public Transform firePoint;
-    public Armas armaEquipada;
-    public float timer;
-    private GameObject lastHitEnemy;
 
+    public Armas armaEquipada;
+
+    public float timer;
     void Update()
     {
-<<<<<<< HEAD
-        if (Input.GetMouseButtonDown(0) && timer > armaEquipada.CadenciaDeTiro)
-=======
-        if (armaEquipada.Automatica)
-        {
-            if (Input.GetKey(KeyCode.Mouse0) && timer > armaEquipada.CadenciaDeTiro)
-            {
-                Shoot();
-            }
-            else if (timer <= armaEquipada.CadenciaDeTiro)
-            {
-                timer = timer + Time.deltaTime;
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.Mouse0) && timer > armaEquipada.CadenciaDeTiro)
->>>>>>> f2ae74881a63f8f91bd04d17ee807fa14ed2b898
+        if (Input.GetKeyDown(KeyCode.Mouse0) && timer > armaEquipada.CadenciaDeTiro)
         {
             Shoot();
         }
         else if (timer <= armaEquipada.CadenciaDeTiro)
         {
-            timer += Time.deltaTime;
+            timer = timer + Time.deltaTime;
         }
 
-
         Vector3 vectormouse = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
         float angle = Mathf.Atan2(vectormouse.y, vectormouse.x) * Mathf.Rad2Deg;
+        //print(angle);
+
         transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 
     void Shoot()
     {
         timer = 0;
-
         for (int i = 0; i < armaEquipada.NumeroDeBalasPorDisparo; i++)
         {
-<<<<<<< HEAD
-            GameObject bullet = Instantiate(armaEquipada.TipoDeMunicíon, firePoint.position, Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z + Random.Range(-armaEquipada.Dispersión, armaEquipada.Dispersión)));
-=======
-            GameObject bullet = Instantiate(armaEquipada.TipoDeMunicíon, firePoint.position, Quaternion.Euler(0,0,transform.rotation.z + Random.Range(-armaEquipada.Dispersión * 2, armaEquipada.Dispersión * 2)));
->>>>>>> f2ae74881a63f8f91bd04d17ee807fa14ed2b898
+            GameObject bullet = Instantiate(armaEquipada.TipoDeMunicíon, firePoint.position, Quaternion.Euler(0, 0, transform.rotation.z + Random.Range(-armaEquipada.Dispersión, armaEquipada.Dispersión)));
             Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
             rb.velocity = firePoint.up * armaEquipada.VelocidadDeLasBalas;
 
-            // Destruye la bala después de 1 segundo.
-            Destroy(bullet, armaEquipada.AlcanceSegundos);
+            // Destruye la bala después de 1 segundos.
+            Destroy(bullet, armaEquipada.AlcanceSegundos); // 1f representa 1 segundos.
         }
-    }
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Enemigo"))
-        {
-            // Comprobar si es el mismo enemigo que fue golpeado anteriormente
-            if (collision.gameObject == lastHitEnemy)
-            {
-                Destroy(collision.gameObject); // Destruir al enemigo si ha sido golpeado dos veces
-            }
-            else
-            {
-                lastHitEnemy = collision.gameObject;
-            }
-        }
     }
 }
