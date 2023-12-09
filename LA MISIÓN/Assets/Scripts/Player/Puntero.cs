55f4242d81c68c165837d6771fe36716d8de354c
Varano3 <jesusvararte@gmail.com>
2023-12-09 10:33:25 +0100

    input change

--- LA MISIÓN/Assets/Scripts/Player/Puntero.cs
+++ LA MISIÓN/Assets/Scripts/Player/Puntero.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Puntero : MonoBehaviour
 {
+    private void Awake()
+    {
+        for (int i = 0; i < Gamepad.all.Count; i++)
+        {
+            Debug.Log(Gamepad.all[i].name);
+        }
+    }
     void Update()
     {
         transform.position = Input.mousePosition;
-
-        //print(Input.GetKey());
     }
 }
